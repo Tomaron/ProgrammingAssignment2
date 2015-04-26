096f4b699fa6bfc8e21824f2c016543d95f1ee68
Tomaron <maritobee@yahoo.com>
2015-04-26 04:12:22 -0500

    add modified cache file

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,33 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { ##makeCacheMatrix stores list of functions
+        m <- NULL ## m variable is assigned NULL
+        set <- function(y) { ## function changes the stored vector in main function
+                x <<- y ##substitutes vector x with y in the main function
+                m <<- NULL ##updates the NULL value of m
+        }
+        get <- function() x ##returns vector x stored in the main function
+        setsolve <- function(solve) m <<- solve ##setsolve stores the value of the input in m variable
+        getsolve <- function() m ##getsolve returns the stored value in m variable
+        list(set = set, get = get, 
+             setsolve = setsolve,
+             getsolve = getsolve) ## list stores all functions in function makeVector
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {    ## Verifies and return the matrix that is the inverse of 'x'
+	            m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
+               inv <- cacheSolve(x) ## gets and assign the cached data to inv
+               inv   ## returns the inverse matrix value of x
 }
